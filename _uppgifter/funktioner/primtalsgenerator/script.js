2a72cee734b025d0f751f8c400b4d15e9620a84b
crilab <62134235+crilab@users.noreply.github.com>
2025-12-08 11:32:02 +0000

    fixed freeze on large numbers

--- _uppgifter/funktioner/primtalsgenerator/script.js
+++ _uppgifter/funktioner/primtalsgenerator/script.js
@@ -1,9 +1,10 @@
 async function main() {
 
-    function isPrime(num) {
+    async function isPrime(num) {
         if (num < 2) return false;
         
         for (let i = 2; i <= Math.sqrt(num); i++) {
+            await sleep(0)
             if (num % i === 0) return false;
         }
         
@@ -13,7 +14,7 @@ async function main() {
     const stop = int(await input('Stop value: '));
 
     for (let i = 0; i < stop; i++) {
-        if (isPrime(i))
+        if (await isPrime(i))
             print('- ' + i);
     }
 }
